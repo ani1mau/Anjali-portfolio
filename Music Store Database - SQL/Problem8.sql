ecd1193313604f96c7681ffad72d13aa440b1b6c
ani1mau <anjalimaurya3000@gmail.com>
2025-10-23 16:46:26 +0530

    Delete Music Store Database - SQL/Problem8.sql

--- Music Store Database - SQL/Problem8.sql
+++
@@ -1,6 +0,0 @@
--- track which songs have duration greater than average duartion
-
-SELECT name, milliseconds/60000 AS Duration_in_sec
-FROM track
-WHERE milliseconds > ( SELECT avg(milliseconds) FROM track)
-
